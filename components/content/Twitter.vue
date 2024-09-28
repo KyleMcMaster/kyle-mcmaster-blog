d551aa3821bf3a85c7b07d0311f7f320584b3f5a
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2024-09-27 21:34:49 -0400

    Add NimblePros link and twitter embed

---
+++ components/content/Twitter.vue
@@ -0,0 +1,9 @@
+<!--https://github.com/DannyFeliz/vue-tweet
+// "vue-tweet": "2.3.1"--> 
+<!-- <script setup lang="ts">
+import Tweet from "vue-tweet";
+</script>
+
+<template>
+  <Tweet tweet-id="1530240085807054848" />
+</template> -->
