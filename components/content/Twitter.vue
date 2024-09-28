7f74eea4636727e620e5f3dea159df7d11e54830
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2024-09-27 21:39:08 -0400

    Fix typo, remove unused vue file

--- components/content/Twitter.vue
+++
@@ -1,9 +0,0 @@
-<!--https://github.com/DannyFeliz/vue-tweet
-// "vue-tweet": "2.3.1"--> 
-<!-- <script setup lang="ts">
-import Tweet from "vue-tweet";
-</script>
-
-<template>
-  <Tweet tweet-id="1530240085807054848" />
-</template> -->
